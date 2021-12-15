d3825b9ab2b40b400b02d2bec6ac0222d7ec1a60
gary <gary.white@lodgit.com>
2021-12-15 13:13:34 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+function divisibleBy(numbers, divisor){
+
+}
+
+console.log(divisibleBy([1,2,3,4,5,6], 2)); // Return [2, 4, 6]
+console.log(divisibleBy([1,2,3,4,5,6], 3)); // Return [3, 6]
+console.log(divisibleBy([0,1,2,3,4,5,6], 4)); // Return [0, 4]
+console.log(divisibleBy([0], 4)); // Return [0]
+console.log(divisibleBy([1,3,5], 2)); // Return []
