ea65b82366ca638f9d62ae6ea8234676fe6ddb0d
gary <gary.white@lodgit.com>
2021-12-16 13:52:22 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function divisibleBy(numbers, divisor){
-
+ let myArr = [];
+ for (let i = 0; i < numbers.length; i++){
+     if(numbers[i] % divisor == 0) {
+        myArr.push(numbers[i])
+     }
+ }
+ return myArr;
 }
 
 console.log(divisibleBy([1,2,3,4,5,6], 2)); // Return [2, 4, 6]
